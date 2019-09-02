adab23afa22362847c898926388f74bae24aa5cc
Christian Hoggard <christianhoggard@gmail.com>
2019-09-02 11:42:21 +0100

    Add files via upload

---
+++ setup.R
@@ -0,0 +1,3 @@
+if(!require("Momocs")) install.packages('Momocs', repos='http://cran.us.r-project.org') 
+if(!require("tidyverse")) install.packages('tidyverse', repos='http://cran.us.r-project.org') 
+if(!require("cowplot")) install.packages('cowplot', repos='http://cran.us.r-project.org')
